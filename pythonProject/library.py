ef24e6b50c30c03478e40496d241e08ef05e3446
Fovezik <68861579+Fovezik@users.noreply.github.com>
2024-04-30 17:49:38 +0200

    working audioFile, simple function of loading audio sample

--- pythonProject/library.py
+++ pythonProject/library.py
@@ -2,9 +2,14 @@ import scikit_build_example as s
 
 
 def test(a, b):
-    print(a, " + ", b, " = ", s.add(a, b))
-    print(a, " - ", b, " = ", s.subtract(a, b))
-    print(b, " - ", a, " = ", s.subtract(b, a))
+    print()
+    print("*****************  ", a, " + ", b, " = ", s.add(a, b))
+    print("* Test Function *  ", a, " - ", b, " = ", s.subtract(a, b))
+    print("*****************  ", b, " - ", a, " = ", s.subtract(b, a))
+    print(), print()
 
 
 test(1000, 1500)
+
+s.audio("C:\\Users\\Lukasz\\Desktop\\Techniki_programowania\\Projekt3_example\\scikit_build_example\\AudioFile\\examples\\test-audio.wav")
+
--- scikit_build_example/src/main.cpp
+++ scikit_build_example/src/main.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <matplot/matplot.h>
+#include <AudioFile.h>
 #include <cmath>
 #include <string>
 #include <vector>
@@ -12,6 +13,23 @@ int add(int i, int j) {
 }
 int subtract(int i, int j) { 
     return i - j; 
+}
+
+void loadAudioFileAndPrintSummary(std::string filePath) {
+    
+    std::cout << "*********************************" << std::endl;
+    std::cout << "Load Audio File and Print Summary" << std::endl;
+    std::cout << "*********************************" << std::endl << std::endl;
+
+    AudioFile<float> a;
+    bool loadedOK = a.load(filePath);
+    assert(loadedOK);
+
+    std::cout << "Bit Depth: " << a.getBitDepth() << std::endl;
+    std::cout << "Sample Rate: " << a.getSampleRate() << std::endl;
+    std::cout << "Num Channels: " << a.getNumChannels() << std::endl;
+    std::cout << "Length in Seconds: " << a.getLengthInSeconds() << std::endl;
+    std::cout << std::endl;
 }
 
 namespace py = pybind11;
@@ -36,6 +54,10 @@ PYBIND11_MODULE(_core, m) {
 
     m.def ("subtract", &subtract, R"pbdoc(
         Substract two numbers.
+    )pbdoc");
+
+    m.def("audio", &loadAudioFileAndPrintSummary, R"pbdoc(
+        Load Audio File and Print Summary.
     )pbdoc");
 
 #ifdef VERSION_INFO
